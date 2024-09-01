f83686a2458416b9b05af1de94d93ad70dcdafbe
jwp23325 <jwp23325@email.vccs.edu>
2024-09-01 16:36:14 -0400

    Add project files.

---
+++ CSC 223 Review Project.cpp
@@ -0,0 +1,10 @@
+// CSC 223 Review Project.cpp : This file contains the code for the review project which will be on three branches: PointType Class, CircleType Class, CyclinderType Class
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "CSC 223 Review Project\n";
+}
+
