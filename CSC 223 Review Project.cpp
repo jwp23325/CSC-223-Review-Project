680e89c654e95682e57576827436175927289914
jwp23325 <jwp23325@email.vccs.edu>
2024-09-01 18:48:40 -0400

    Added a Setters and Getters test

--- CSC 223 Review Project.cpp
+++ CSC 223 Review Project.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main()
 {
-    
+    // Default constructor test
     cout << "From default constructor" << endl;
     PointType<int> p1I;
     PointType<double> p1D;
@@ -17,6 +17,7 @@ int main()
     cout << "Double point: ";
     p1D.print();
 
+    // Parameterized constructor test
     cout << "/nConstructor with parameters" << endl;
     PointType<int> p2I(3, 5);
     PointType<double> p2D(3.5, 5.5);
@@ -25,6 +26,7 @@ int main()
     cout << "Double point: ";
     p2D.print();
 
+    // Distance calculation test
     cout << "\n(integer) Distance = ";
     double distance = p1I - p2I;
     cout << distance << endl;
@@ -32,6 +34,16 @@ int main()
     cout << "\n(double) Distance = ";
     distance = p1D - p2D;
     cout << distance << endl;
+
+    // Setters Test
+    p1I.setX(7);
+    p1I.setY(9);
+    p1D.setX(4.4);
+    p1D.setY(6.6);
+
+    // Getters Test
+    cout << "\nNew integer point: (" << p1I.getX() << ", " << p1I.getY() << ")" << endl;
+    cout << "New integer point: (" << p1D.getX() << ", " << p1D.getY() << ")" << endl;
 
 
 }
